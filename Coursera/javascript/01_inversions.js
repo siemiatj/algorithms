3a52b9ee96a8456ef0d854141760474eb8c504d1
siemiatj <kuba@saucelabs.com>
2016-06-19 23:57:49 +0200

    - add test file

--- Coursera/javascript/01_inversions.js
+++ Coursera/javascript/01_inversions.js
@@ -1,27 +1,41 @@
+const path = require('path');
+const pathArg = process.argv[2];
+const filePath = path.resolve(__dirname, pathArg);
 
+console.log('ARGS: ', pathArg, filePath);
+
+var test = [34, 203, 3, 746, 200, 984, 198, 764, 9];
 
 function mergeSort(arr) {
-    if (arr.length < 2) {
-      return arr;
-    }
- 
-    let middle = parseInt(arr.length / 2),
-      left   = arr.slice(0, middle),
-      right  = arr.slice(middle);
- 
-    return merge(mergeSort(left), mergeSort(right));
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let middle = parseInt(arr.length / 2),
+    left   = arr.slice(0, middle),
+    right  = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
  
 function merge(left, right) {
-    var result = [];
- 
-    while (left.length && right.length) {
-        if (left[0] <= right[0]) {
-            result.push(left.shift());
-        } else {
-            result.push(right.shift());
-        }
-    }
+  var result = [];
 
-    return result.concat(left.slice(il)).concat(right.slice(ir));
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length)
+    result.push(left.shift());
+
+  while (right.length)
+    result.push(right.shift());
+
+  return result;
 }
+ 
+console.log(mergeSort(test));
