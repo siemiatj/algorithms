50bc11635209b8d1305e7dd1a7fa1726cf04a200
siemiatj <kuba@saucelabs.com>
2016-06-20 16:01:07 +0200

    - add haskell merge sort

---
+++ Coursera/haskell/mergesort.hs
@@ -0,0 +1,13 @@
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = merge (mergeSort left) (mergeSort right)
+  where left = take (length xs `div` 2) xs
+        right = drop (length xs `div` 2) xs
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+  | (x<= y) = x:(merge xs (y:ys))
+  | otherwise = y:(merge (x:xs) ys)
