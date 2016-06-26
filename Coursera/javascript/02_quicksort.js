49d294b2f4ef7ae9c4976ff7f314f677aa8e01de
siemiatj <kuba@saucelabs.com>
2016-06-26 19:18:21 +0200

    - quicksort partitioning

---
+++ Coursera/javascript/02_quicksort.js
@@ -0,0 +1,37 @@
+let toSort = [3,8,2,5,1,4,7,6];
+
+const quicksort = function(array) {
+  if (array.length < 2) {
+    return array;
+  }
+
+  const partition = function(ar, l, r) {
+    let pivot = ar[l];
+    let i = l + 1;
+    let j = l + 1;
+
+    for (j; j < r; j += 1) {
+      if (ar[j] < pivot) {
+        swap(ar, j, i);
+        i += 1;
+      }
+    }
+
+    swap(ar, 0, i - 1);
+  };
+
+  const swap = function(ar, a, b) {
+    let first = ar[a];
+    let second = ar[b];
+
+    ar[a] = second;
+    ar[b] = first;
+  };
+
+  partition(array, 0, array.length);
+
+  return array;
+}
+
+console.log(quicksort(toSort));
+
