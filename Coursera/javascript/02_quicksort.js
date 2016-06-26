42f62d37f7d2f7b9e495943c113561382374cbf3
siemiatj <kuba@saucelabs.com>
2016-06-26 19:54:32 +0200

    - full quicksort, seems to be working fine

--- Coursera/javascript/02_quicksort.js
+++ Coursera/javascript/02_quicksort.js
@@ -6,6 +6,9 @@ const quicksort = function(array) {
   }
 
   const partition = function(ar, l, r) {
+    if (r - l < 2) {
+      return;
+    }
     let pivot = ar[l];
     let i = l + 1;
     let j = l + 1;
@@ -17,7 +20,10 @@ const quicksort = function(array) {
       }
     }
 
-    swap(ar, 0, i - 1);
+    swap(ar, l, i - 1);
+
+    partition(ar, l, i);
+    partition(ar, i, r);
   };
 
   const swap = function(ar, a, b) {
@@ -34,4 +40,3 @@ const quicksort = function(array) {
 }
 
 console.log(quicksort(toSort));
-
