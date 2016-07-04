f51910d0b13361ff463b1cd0bfa9cfe09e48c42f
siemiatj <kuba@saucelabs.com>
2016-07-05 00:11:28 +0200

    [WiP] - trying to simplify the solution

--- Coursera/javascript/03_min_cut.js
+++ Coursera/javascript/03_min_cut.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
-const pathArg = process.argv[2];
-const filePath = path.resolve(__dirname, pathArg);
+const PATHARG = process.argv[2];
+const RETRIES = parseInt(process.argv[3], 10);
+const filePath = path.resolve(__dirname, PATHARG);
 const lineReader = readline.createInterface({
   input: fs.createReadStream(filePath)
 });
 
 let MIN_CUTS = null;
-// let verticesObject = {};
-// let availableVertices = [];
+let verticesTmpObject = {};
 let verticesArray = [];
 let edgesArray = [];
 let parsedLine;
@@ -18,89 +18,79 @@ lineReader.on('line', function (line) {
   parsedLine = line.split(/\s+/);
   let vertexNo = parsedLine.shift();
   verticesArray.push(vertexNo);
+  verticesTmpObject[vertexNo] = {};
 
   parsedLine.forEach((l, idx) => {
-    // parsedLine[idx] = parseInt(l, 10);
-    edgesArray.push([vertexNo, l]);
+    verticesTmpObject[vertexNo][l] = true;
   });
-  // verticesObject[vertexNo] = parsedLine;
-
 }).on('close', () => {
+  // remove duplicated edges
+  for (let n in verticesTmpObject) {
+    for (let m of Object.keys(verticesTmpObject[n])) {
+      edgesArray.push([n, m]);
+
+      delete verticesTmpObject[m][n];
+    }
+  }
   let verticesCopy, allVerticesCopy;
 
-  for (let i=0; i< 2; i+= 1) {
-    verticesCopy = Object.assign({}, verticesObject);
-    allVerticesCopy = availableVertices.slice());
+  for (let i=0; i< RETRIES; i+= 1) {
+    verticesCopy = verticesArray.slice()
+    edgesCopy = edgesArray.slice();
 
-    findMinCut(verticesCopy, allVerticesCopy);
+    findMinCut(verticesCopy, edgesCopy);
   }
   console.log('Minimum cut number: ', MIN_CUTS);
 });
 
-const findMinCut = function(vertices, allVertices) {
-  // console.log('DATA: ', vertices, allVertices);
-  let lengthLeft = Object.keys(vertices).length;
-  if (lengthLeft === 2) {
-    let vertex1 = allVertices[0];
-
-    // console.log('VERTEX: ', vertex1, allVertices, vertices);
-
-    if (MIN_CUTS === null || (vertices[vertex1].length < MIN_CUTS)) {
-      MIN_CUTS = vertices[vertex1].length;
+const findMinCut = function(vertices, edges) {
+  if (vertices.length === 2) {
+    if (MIN_CUTS === null || (edges < MIN_CUTS)) {
+      MIN_CUTS = edges.length;
     }
     return;
   }
 
-  const getEdge = function() {
-    let n1 = Math.floor(Math.random() * (allVertices.length));
-    let len2 = allVertices[n1].length;
-    let n2 = Math.floor(Math.random() * (len2));
+  // remove edge and vertex at one end of this edge
+  const removeEdge = function() {
+    let m = Math.floor(Math.random() * (edges.length));
+    let edge = edges[m];
+    let n1 = edges[m][0];
+    let n2 = edges[m][1];
 
-    n1 = parseInt(allVertices[n1], 10);
+    edges.splice(m, 1);
+    vertices = vertices.filter(v => {
+      return v != n2;
+    });
 
-    return [n1, vertices[n1][n2]];
-  };
-
-  const getEdges = function(n) {
-    return vertices[n];
+    return [n1, n2];
   };
 
   const contract = function(edge) {
     let n1 = edge[0];
     let n2 = edge[1];
-    let n1Edges = getEdges(n1);
-    let n2Edges = getEdges(n2);
-    let mergedEdges = n1Edges.concat(n2Edges);
 
-    mergedEdges = mergedEdges.filter(e => {
-      return e !== n1 && e !== n2;
+    edges.forEach(e => {
+      if (e[1] == n2) {
+        e[1] = n1;
+      } else if (e[0] == n2) {
+        e[0] = n1;
+      }
     });
-
-    vertices[n1] = mergedEdges;
-    delete vertices[n2];
   };
 
-  const removeOldVertex = function(edge) {
-    for (let v in vertices) {
-      let vert = vertices[v];
+  const removeLoops = function(edge) {
+    let n1 = edge[0];
 
-      vert.forEach((v, idx) => {
-        if (v === edge[1]) {
-          vert[idx] = parseInt(edge[0], 10);
-        }
-      });
-    }
-
+    edges = edges.filter(e => {
+      return (e[0] != n1 && e[1] != n1);
+    });
   };
-  let edge = getEdge();
+
+  let edge = removeEdge();
 
   contract(edge);
-  removeOldVertex(edge);
+  removeLoops(edge);
 
-  allVertices = allVertices.filter(k => {
-    return parseInt(k, 10) !== parseInt(edge[1], 10);
-  });
-
-  // MIN_CUT +=1;
-  findMinCut(vertices, allVertices);
+  findMinCut(vertices, edges);
 };
