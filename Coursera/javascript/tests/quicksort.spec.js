617e86a480d459d2e731bae9ff2d8f025da90617
siemiatj <kuba@saucelabs.com>
2016-07-24 23:42:29 +0200

    - test case for quicksort

---
+++ Coursera/javascript/tests/quicksort.spec.js
@@ -0,0 +1,32 @@
+let quickSortFst = require("../quicksort.js");
+let quickSortLast = require("../quicksort_2.js");
+let quickSortMed = require("../quicksort_3.js");
+
+describe('Sorting an unsorted Array using quicksort with first element as pivot', function () {
+
+  var unsortedLength = null;
+  var sortedArray = null;
+
+  beforeEach(function() {
+    var unsortedArray = [10, 8, 1, 3, 5, 7, 6, 9];
+    unsortedLength = unsortedArray.length;
+    quickSortFst(unsortedArray);
+    sortedArray = unsortedArray;
+  });
+
+  it('the sorted array will not be null.', function () {
+    expect(sortedArray).not.toBe(null);
+  });
+
+  it('a sorted array of the same length will be returned.', function () {
+    expect(sortedArray.length).toEqual(unsortedLength);
+  });
+
+  it('the first element will be 1', function () {
+    expect(sortedArray[0]).toEqual(1);
+  });
+
+  it('the last element will be 10', function () {
+    expect(sortedArray[sortedArray.length - 1]).toEqual(10);
+  });
+});
