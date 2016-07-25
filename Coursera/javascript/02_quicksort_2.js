9ce620713b4d1ade069b349cc0195a0dc518e269
siemiatj <kuba@saucelabs.com>
2016-07-26 00:23:24 +0200

    - test for quicksort with last element

--- Coursera/javascript/02_quicksort_2.js
+++ Coursera/javascript/02_quicksort_2.js
@@ -6,58 +6,12 @@ const filePath = path.resolve(__dirname, pathArg);
 const lineReader = readline.createInterface({
   input: fs.createReadStream(filePath)
 });
+const quickSort = require('./lib/quicksort_last').quickSort;
+const toSortArray = [];
 
-let toSortArray = [];
-let totalNumberOfComparisons = 0;
-
-lineReader.on('line', function (line) {
+lineReader.on('line', (line) => {
   toSortArray.push(parseInt(line, 10));
 }).on('close', () => {
-  quickSort(toSortArray);
-  console.log('Total number of comparisons (last element as pivot): ', totalNumberOfComparisons);
+  const comparisons = quickSort(toSortArray);
+  console.log('Total number of comparisons (last element as pivot): ', comparisons);
 });
-
-/*
- * Last array element as pivot
- */
-const quickSort = function(array) {
-  if (array.length < 2) {
-    return;
-  }
-
-  const partition = function(ar, l, r) {
-    let len = r - l;
-
-    if (len < 2) {
-      return;
-    }
-    totalNumberOfComparisons += len - 1;
-
-    let pivot = ar[r - 1];
-    let i = l + 1;
-    let j = i;
-
-    swap(ar, r-1, l);
-
-    for (j; j < r; j += 1) {
-      if (ar[j] < pivot) {
-        swap(ar, j, i);
-        i += 1;
-      }
-    }
-
-    swap(ar, l, i - 1);
-
-    partition(ar, l, i - 1);
-    partition(ar, i, r);
-  };
-
-  const swap = function(ar, a, b) {
-    let tmp = ar[a];
-
-    ar[a] = ar[b];
-    ar[b] = tmp;
-  };
-
-  partition(array, 0, array.length);
-}
--- Coursera/javascript/test/quicksort.spec.js
+++ Coursera/javascript/test/quicksort.spec.js
@@ -2,6 +2,7 @@
 
 const expect = require('chai').expect;
 const quickSortFst = require('../lib/quicksort_first.js').default;
+const quickSortLast = require('../lib/quicksort_last.js').default;
 
 describe('Sorting an unsorted Array using quicksort with first element as pivot', () => {
   let unsortedLength = null;
@@ -11,6 +12,34 @@ describe('Sorting an unsorted Array using quicksort with first element as pivot'
     const unsortedArray = [10, 8, 1, 3, 5, 7, 6, 9];
     unsortedLength = unsortedArray.length;
     quickSortFst(unsortedArray);
+    sortedArray = unsortedArray;
+  });
+
+  it('the sorted array will not be null.', () => {
+    expect(sortedArray).to.not.equal(null);
+  });
+
+  it('a sorted array of the same length will be returned.', () => {
+    expect(sortedArray.length).to.equal(unsortedLength);
+  });
+
+  it('the first element will be 1', () => {
+    expect(sortedArray[0]).to.equal(1);
+  });
+
+  it('the last element will be 10', () => {
+    expect(sortedArray[sortedArray.length - 1]).to.equal(10);
+  });
+});
+
+describe('Sorting an unsorted Array using quicksort with the last element as pivot', () => {
+  let unsortedLength = null;
+  let sortedArray = null;
+
+  beforeEach(() => {
+    const unsortedArray = [10, 8, 1, 3, 5, 7, 6, 9];
+    unsortedLength = unsortedArray.length;
+    quickSortLast(unsortedArray);
     sortedArray = unsortedArray;
   });
 
