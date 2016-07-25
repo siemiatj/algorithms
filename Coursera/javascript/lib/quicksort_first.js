0916fd937bf1f234db67a65badd35d323b4ea71a
siemiatj <kuba@saucelabs.com>
2016-07-26 00:03:42 +0200

    - move quicksort code to lib

---
+++ Coursera/javascript/lib/quicksort_first.js
@@ -0,0 +1,55 @@
+let totalNumberOfComparisons = 0;
+
+/*
+ * First array element as pivot
+ */
+function quickSort(array) {
+  if (array.length < 2) {
+    return;
+  }
+
+  const swap = function (ar, a, b) {
+    const tmp = ar[a];
+
+    ar[a] = ar[b];
+    ar[b] = tmp;
+  };
+
+  const partition = function (ar, l, r) {
+    const len = r - l;
+
+    if (len < 2) {
+      return;
+    }
+    totalNumberOfComparisons += len - 1;
+
+    const pivot = ar[l];
+    let i = l + 1;
+    let j = i;
+
+    for (j; j < r; j += 1) {
+      if (ar[j] < pivot) {
+        swap(ar, j, i);
+        i += 1;
+      }
+    }
+
+    swap(ar, l, i - 1);
+
+    partition(ar, l, i - 1);
+    partition(ar, i, r);
+  };
+
+  partition(array, 0, array.length);
+}
+
+function quickSortAndCount(arr) {
+  quickSort(arr);
+
+  return totalNumberOfComparisons;
+}
+
+module.exports = {
+  default: quickSort,
+  quickSort: quickSortAndCount
+};
